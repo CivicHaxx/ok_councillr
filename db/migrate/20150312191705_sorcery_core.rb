4a3171f943a3914710d2064287ffe935098c7838
emilyjfan <emilyjustinefan@gmail.com>
2015-03-12 15:22:30 -0400

    Adding sorcery gem. Creating User model.

---
+++ db/migrate/20150312191705_sorcery_core.rb
@@ -0,0 +1,17 @@
+class SorceryCore < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email,            :null => false
+      t.string :crypted_password 
+      t.string :salt
+      t.string :first_name 				:null => false
+      t.string :last_name					:null => false
+      t.string :postal_code				
+      t.string :address	
+
+      t.timestamps
+    end
+
+    add_index :users, :email, unique: true
+  end
+end
