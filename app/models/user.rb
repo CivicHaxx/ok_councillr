8cf8b20a9544dec59954f895c85f5aca4f823fd8
emilyjfan <emilyjustinefan@gmail.com>
2015-03-12 18:10:31 -0400

    Add association User has many Items thru UserVotes

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   has_many :user_votes
+  has_many :items, through: :user_votes
 
   validates :password, length: { minimum: 8}
   validates :password, confirmation: true
