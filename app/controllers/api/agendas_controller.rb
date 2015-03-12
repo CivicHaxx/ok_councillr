457a3d3165fcc7fbe0fb5e533e0a417c84890be5
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-12 18:46:15 -0400

    Add Agenda's Reader Action Methods As APIs
    Add in both the reeder action methods, show and index, in order use them
    for read only APIs, on the Agenda. Also made sure that JSON would be the
    only format rendering since the API is going to be render as a JSON.

--- app/controllers/api/agendas_controller.rb
+++ app/controllers/api/agendas_controller.rb
@@ -1,2 +1,13 @@
 class Api::AgendasController < ApplicationController
+	def index
+		@agendas = Agenda.all
+
+		render json: @agendas
+	end
+
+	def show
+		@agenda = Agenda.find(params[:id])
+
+		render json: @agenda
+	end
 end
