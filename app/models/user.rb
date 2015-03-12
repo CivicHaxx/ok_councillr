192f5fba46cb1ec01e947d7a772cbe557659cc93
emilyjfan <emilyjustinefan@gmail.com>
2015-03-12 17:27:20 -0400

    Adding validations to User model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
   has_many :user_votes
+
+  validates :password, length: { minimum: 8}
+  validates :password, confirmation: true
+  validates :password_confirmation, presence: true 
+  validates :email, uniqueness: true 
 end
