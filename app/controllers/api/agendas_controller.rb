238e3f4bf228c75fca93bf7a8583302af13b406a
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-12 18:36:39 -0400

    Add API Namespace Routing & Agenda
    Add in the APIs namespace for both the routing and the agenda because
    this URL will be used to allow the user access to the data on the site.

---
+++ app/controllers/api/agendas_controller.rb
@@ -0,0 +1,2 @@
+class Api::AgendasController < ApplicationController
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :agendas, only: [:index, :show]
+  end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
