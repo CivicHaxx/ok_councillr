96d87f9ab0c49e8c6bfb4d00165e2b31a0c84ded
emilyjfan <emilyjustinefan@gmail.com>
2015-03-13 19:27:23 -0400

    Created User Session controller, view and routes

---
+++ app/controllers/user_sessions_controller.rb
@@ -0,0 +1,19 @@
+class UserSessionsController < ApplicationController
+  def new
+  	@user = User.new 
+  end
+
+  def create
+  	if @user = login(params[:email], params[:password])
+  		redirect_back_or_to(:users, notice: 'Login successful')
+  	else
+  		flash.now[:alert] = 'Login failed'
+  		render action: 'new'
+  	end
+  end
+
+  def destroy
+  	logout 
+  	redirect_to(:users, notice: 'Logged out!')
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,12 @@ Rails.application.routes.draw do
   end
 
   resources :users, only: [:index, :update, :edit, :destroy]
+  resources :user_sessions 
+
   get "signup" => "users#new", :as => :signup
   get "myvotes/:id" => "users#show", :as => :myvotes
+  get 'login' => 'user_sessions#new', :as => :login
+  post 'logout' => 'user_sessions#destroy', :as => :logout
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
