12fbe752fa95333380a683e586c295d98865fb5f
emilyjfan <emilyjustinefan@gmail.com>
2015-03-13 18:25:15 -0400

    Fix error with user#index route for signup view

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,8 @@ Rails.application.routes.draw do
     resources :items, only: [:index, :show]
   end
 
-  resources :users, only: [:show, :update, :edit, :destroy]
-  get 'signup' => "users#new", as: :signup
+  resources :users, only: [:index, :show, :update, :edit, :destroy]
+  get "signup" => "users#new", :as => :signup
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
