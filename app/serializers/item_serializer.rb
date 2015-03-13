e0bc0caf97213614615f3bf6438ba80e7a8bd4d3
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-13 16:02:38 -0400

    Add Recommendations & Sections To Item Serializer

--- app/serializers/item_serializer.rb
+++ app/serializers/item_serializer.rb
@@ -1,5 +1,5 @@
 class ItemSerializer < ActiveModel::Serializer
-  attributes :id, :title, :ward, :number
+  attributes :id, :title, :ward, :number, :recommendations, :sections
 
   has_one :item_type
 end
