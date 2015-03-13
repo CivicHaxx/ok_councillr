c0da71dc6c92396fbfd13f635d19ed20b45889b2
emilyjfan <emilyjustinefan@gmail.com>
2015-03-13 18:24:46 -0400

    Create views for User signup & editing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,12 +39,11 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(
       :email,
-      :crypted_password,
-      :salt,
+      :password,
+      :password_confirmation,
       :first_name,
       :last_name,
-      :postal_code,
-      :address
+      :postal_code
     )
   end
 end
