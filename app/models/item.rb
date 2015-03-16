b82e89b5c9200a2a75d638e0830686cdc6d06a13
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-16 15:45:34 -0400

    Add Dependent Destroy To All Models

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,7 +4,7 @@ class Item < ActiveRecord::Base
 	belongs_to :item_type
 	belongs_to :origin, polymorphic: true
 
-	has_many :user_votes
-	has_many :motion
+	has_many :user_votes, dependent: :destroy
+	has_many :motion, dependent: :destroy
 	has_and_belongs_to_many :wards
 end
