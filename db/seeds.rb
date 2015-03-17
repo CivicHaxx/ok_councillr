50bf2196c3b0a5acf90a295e9b2e6d3ca6855b25
CivicImages <kobewka@gmail.com>
2015-03-17 15:32:25 -0400

    dropping old fake seed loop

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,15 +1,24 @@
 require "councillors_wards"
 
+puts "Destroying Councillors".red
 Councillor.destroy_all
+puts "Destroying Committees".red
 Committee.destroy_all
+puts "Destroying Users".red
 User.destroy_all
-Agenda.destroy_all 
-Item.destroy_all
+#Agenda.destroy_all 
+#Item.destroy_all
+puts "Destroying Motions".red
 Motion.destroy_all
+puts "Destroying CouncillorVotes".red
 CouncillorVote.destroy_all
+puts "Destroying UserVotes`"
 UserVote.destroy_all
+puts "Destroying ItemTypes".red
 ItemType.destroy_all
+puts "Destroying MotionTypes".red
 MotionType.destroy_all
+puts "Destroying Wards".red
 Ward.destroy_all
 
 prefix           = %w(EX CD GM PG)
@@ -22,14 +31,14 @@ wards            = []
 people           = %w(👦 👧 👨 👩)
 
 puts "Creating Item Types".blue
-item_types = ItemType.create([
+item_types = ItemType.create!([
 	{ name: 'Action' }, 
 	{ name: 'Information' }, 
 	{ name: 'Presentation' }
 ])
 
 puts "Creating Motion Types".blue
-motion_types = MotionType.create([
+motion_types = MotionType.create!([
 	{ name: 'Adopted' }, 
 	{ name: 'Received' },
 	{ name: 'Amended' }
@@ -37,7 +46,7 @@ motion_types = MotionType.create([
 
 puts "Creating Users".blue
 10.times do
-	users << User.create(
+	users << User.create!(
 		email: Faker::Internet.safe_email,
 		first_name: Faker::Name.first_name,
 		last_name: Faker::Name.last_name,
@@ -51,8 +60,8 @@ end
 
 puts "\nCreating Wards & Councillors".blue
 WARD_INFO.each do |ward|
-	wards << Ward.create({ ward_number: ward[2], name: ward[3] })
-	councillors << Councillor.create({
+	wards << Ward.create!({ ward_number: ward[2], name: ward[3] })
+	councillors << Councillor.create!({
 			first_name: ward[1],
 	    last_name: ward[0],
 	    start_date_in_office: Faker::Date.backward(61),
@@ -69,18 +78,18 @@ WARD_INFO.each do |ward|
 end
 
 print "\nCreating City Council".blue
-@council = Committee.create({
+@council = Committee.create!({
   name: "City Council",
 })
+
 councillors.each do |councillor|
 	@council.councillors << councillor
 end
 print " 👍"
 
-puts " "
-puts "Creating Random Committees".yellow
+puts "\nCreating Random Committees".yellow
 rand(9).times do
-	committee = Committee.create(name: Faker::Company.name)
+	committee = Committee.create!(name: Faker::Company.name)
 
 	5.times do
 		committee.councillors << councillors.sample
@@ -88,63 +97,96 @@ rand(9).times do
 	print "❤️"; print " "
 end
 
-puts "\nCreating Agendas and Items".blue
-Rake::Task['okc:agendas'].execute
+# puts "\nCreating Agendas and Items".blue
+# Rake::Task['okc:agendas'].execute
 
 # TODO: Create fake motions and on each item and fake user votes on each
 #       motion. 
 # NOTE: We are currently building the app so users can vote on items and not 
 #       motions. Perhaps we should change this temporarily.
-
-2.times do 
-	agenda = Agenda.create(
-		date: Faker::Date.between(60.days.ago, Date.today)
-	)
-
-	100.times do |index|
-		current_origin_from = ((index % 2) == 0) ? agenda : councillors.sample
-
-		item = Item.create(
-			title: Faker::Hacker.say_something_smart, 
-			wards: wards[1...rand(wards.count)],
-			number: "#{prefix.sample}#{Faker::Number.number(1)}.#{Faker::Number.number(2)}",
-			sections: {	"Recommendations" => "#{Faker::Lorem.paragraphs(5)}",
-									"Origin" => "#{Faker::Lorem.paragraphs(5)}",
-									"Summary" => "#{Faker::Lorem.paragraphs(5)}",
-									"Background Information" => "#{Faker::Lorem.paragraphs(5)}"},
-      recommendations: "#{Faker::Company.bs} #{Faker::Company.catch_phrase}",
-			item_type_id: item_types.sample.id,
-			origin: current_origin_from
-		)
-
-		rand(20).times do
-			motion_type = motion_types.sample
-			amendment_text = (motion_type.name == "Amended") ? Faker::Lorem.paragraph(4, true, 6) : ""
-
-			motion = Motion.create(
+puts "\nCreating fake motions and votes".yellow
+Agenda.first.items.all.each do |item|
+	rand(5).times do 
+		motion_type    = motion_types.sample
+		amendment_text = (motion_type.name == "Amended") ? Faker::Lorem.paragraph(4, true, 6) : ""
+	
+		motion = Motion.create!(
 				amendment_text: amendment_text,
 				councillor_id: councillors.sample.id,
 				item_id: item.id,
 				motion_type_id: motion_type.id
 			)
-
-			rand(20).times do
-				CouncillorVote.create(
-					vote: councillor_votes.sample,
-					councillor_id: councillors.sample.id,
-					motion_id: motion.id
-				)
-			end
+		rand(10).times do
+			CouncillorVote.create!(
+				vote: councillor_votes.sample,
+				councillor_id: councillors.sample.id,
+				motion_id: motion.id
+			)
 		end
 
-		rand(20).times do
-			UserVote.create(
+		rand(10).times do
+			UserVote.create!(
 				vote: user_votes.sample,
 				user_id: users.sample.id,
 				item_id: item.id
 			)
 		end
-		print "❤️"; print " "
 	end
-print "\n💘 💘 💘\n";
+	print "❤️"; print " "
 end
+
+print "\n💘 💘 💘\n";
+
+
+# 2.times do 
+# 	agenda = Agenda.create(
+# 		date: Faker::Date.between(60.days.ago, Date.today)
+# 	)
+
+# 	100.times do |index|
+# 		current_origin_from = ((index % 2) == 0) ? agenda : councillors.sample
+
+# 		item = Item.create(
+# 			title: Faker::Hacker.say_something_smart, 
+# 			wards: wards[1...rand(wards.count)],
+# 			number: "#{prefix.sample}#{Faker::Number.number(1)}.#{Faker::Number.number(2)}",
+# 			sections: {	"Recommendations" => "#{Faker::Lorem.paragraphs(5)}",
+# 									"Origin" => "#{Faker::Lorem.paragraphs(5)}",
+# 									"Summary" => "#{Faker::Lorem.paragraphs(5)}",
+# 									"Background Information" => "#{Faker::Lorem.paragraphs(5)}"},
+#       recommendations: "#{Faker::Company.bs} #{Faker::Company.catch_phrase}",
+# 			item_type_id: item_types.sample.id,
+# 			origin: current_origin_from
+# 		)
+
+# 		rand(20).times do
+# 			motion_type = motion_types.sample
+# 			amendment_text = (motion_type.name == "Amended") ? Faker::Lorem.paragraph(4, true, 6) : ""
+
+# 			motion = Motion.create(
+# 				amendment_text: amendment_text,
+# 				councillor_id: councillors.sample.id,
+# 				item_id: item.id,
+# 				motion_type_id: motion_type.id
+# 			)
+
+# 			rand(20).times do
+# 				CouncillorVote.create(
+# 					vote: councillor_votes.sample,
+# 					councillor_id: councillors.sample.id,
+# 					motion_id: motion.id
+# 				)
+# 			end
+# 		end
+
+# 		rand(20).times do
+# 			UserVote.create(
+# 				vote: user_votes.sample,
+# 				user_id: users.sample.id,
+# 				item_id: item.id
+# 			)
+# 		end
+# 		print "❤️"; print " "
+# 	end
+# print "\n💘 💘 💘\n";
+# end
