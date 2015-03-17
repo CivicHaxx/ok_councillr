2acf1112a595e2f9830e0328a66085ac5b765a30
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-17 19:36:57 -0400

    Add Method To Display User Votes In Item Index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+	helper_method :display_user_votes_for
 
 	def index
 		@items = Item.all.page params[:page]
@@ -9,4 +10,15 @@ class ItemsController < ApplicationController
 		@user_vote = UserVote.new
 	end
 
+	private 
+	def display_user_votes_for(item)
+		display_user_votes = []
+		user_votes         = %w(Yes No Skip)
+
+		user_votes.each do |user_vote|
+			display_user_votes << item.user_votes.where(vote: user_vote).count
+		end
+
+		display_user_votes.join('-')
+	end
 end
