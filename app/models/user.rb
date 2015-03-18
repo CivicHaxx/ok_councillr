37d159fcf6cb23fbbaaaad72020e29839c481bec
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-18 15:09:22 -0400

    Change Password To Have Empty Passwords On Edit
    Change the password to have an empty passwords on edit since the user
    will not be change their password every time. But also still enforce
    the password length if they decided to enter in a password when editing.

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,10 +6,10 @@ class User < ActiveRecord::Base
 
   validates :first_name, presence: true 
   validates :last_name, presence: true 
-  validates :password, length: { minimum: 8}
-  validates :password, confirmation: true
-  validates :password_confirmation, presence: true 
-  validates :email, uniqueness: true 
+  validates :password, length: { minimum: 8}, unless: Proc.new { |a| a.password.blank? }
+  validates :password, confirmation: true, on: :create
+  validates :password_confirmation, presence: true, on: :create
+  validates :email, uniqueness: true
 
   def has_no_votes?
   	user_votes.count == 0
