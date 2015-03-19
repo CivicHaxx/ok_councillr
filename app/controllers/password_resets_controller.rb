cc94e189837d5a65d517ab047e2604d6a9c9635a
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-19 16:23:06 -0400

    Add Form & Code To Find User & Send Link
    Add form to allow the user to enter into their email to send out a link
    to them in order to reset their password.

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,5 +1,12 @@
 class PasswordResetsController < ApplicationController
+	skip_before_filter :require_login
+
   def create
+  	@user = User.find_by_email(params[:email])
+
+  	@user.deliver_reset_password_instructions! if @user
+
+  	redirect_to :login, notice: 'Instructions have been sent to your email'
   end
 
   def edit
