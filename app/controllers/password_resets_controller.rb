481bbf582367d0a0481f71faba352f8a97b8a18e
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-19 16:24:40 -0400

    Add Edit Form & Code To Change User's Password

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -10,8 +10,31 @@ class PasswordResetsController < ApplicationController
   end
 
   def edit
+  	@token = params[:id]
+  	@user = User.load_from_reset_password_token(params[:id])
+
+  	if @user.blank?
+  		not_authenticated
+  		return
+  	end
   end
 
   def update
+  	@token = params[:id]
+  	@user = User.load_from_reset_password_token(params[:id])
+
+  	if @user.blank?
+  		not_authenticated
+  		return
+  	end
+
+  	@user.password_confirmation = params[:user][:password_confirmation]
+
+  	if @user.change_password!(params[:user][:password])
+      auto_login(@user)
+  		redirect_to root_path, notice: 'Reset Password was successfully'
+  	else
+  		render action: :edit
+  	end
   end
 end
