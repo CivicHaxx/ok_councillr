1789055473a806d90e12bd27dd43f7fa3ad14336
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-19 23:03:37 -0400

    Fix Spacing & Add Items When In Index
    Fix the spacing and add in the items to user votes when the index page
    loads.

--- app/controllers/user_votes_controller.rb
+++ app/controllers/user_votes_controller.rb
@@ -1,34 +1,31 @@
 class UserVotesController < ApplicationController
+  def index
+      @user = current_user
+      @votes = @user.user_votes.includes(:item).page params[:page]
+  end
 
-def index
-    @user = current_user
-    @votes = @user.user_votes.page params[:page]
-end
+  def new	
+  	@user_vote = UserVote.new
+  end
 
-def new	
-	@user_vote = UserVote.new
-end
+  def create
+  	@item = Item.find(params[:item_id])
+    @user_vote = @item.user_votes.build(vote_params)
+  	@user_vote.user = current_user
 
-def create
-	@item = Item.find(params[:item_id])
-  @user_vote = @item.user_votes.build(vote_params)
-	@user_vote.user = current_user
-
-	if @user_vote.save
+  	if @user_vote.save
       redirect_to item_path(@item.next)
     else
-      render 
+      render :new
     end
-end
+  end
 
-private
-
+  private
   def vote_params
-  params.require(:user_vote).permit(
-    :vote,
-    :item_id,
-    :user_id
+    params.require(:user_vote).permit(
+      :vote,
+      :item_id,
+      :user_id
     )
-	end
-	
+  end
 end
