3ff1e60b202d3140b252cd93f347d3b1e2cb7bb6
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-21 23:21:00 -0400

    Add User Activate & Success Email To UserMailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,8 +3,21 @@ class UserMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
+  #   en.user_mailer.activation_needed_email.subject
+  #   en.user_mailer.activation_success_email.subject
   #   en.user_mailer.reset_password_email.subject
-  #
+  def activation_needed_email(user)
+    @user = user
+    @url = activate_user_url
+    mail to: user.email, subject: "Welocme to OK Councillr"
+  end
+
+  def activation_success_email(user)
+    @user = user
+    @url = login_url
+    mail to: user.email, subject: "Your account is now activated"
+  end
+  
   def reset_password_email(user)
   	@user = user
   	@url = edit_password_reset_url(user.reset_password_token)
