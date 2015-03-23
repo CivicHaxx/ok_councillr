a5d47043437e5cee58bf78545839aa6b35df6f9f
emilyjfan <emilyjustinefan@gmail.com>
2015-03-23 19:09:23 -0400

    Update schema for migrations on User table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150322024540) do
+ActiveRecord::Schema.define(version: 20150323220803) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -145,8 +145,7 @@ ActiveRecord::Schema.define(version: 20150322024540) do
     t.string   "salt"
     t.string   "first_name",                      null: false
     t.string   "last_name",                       null: false
-    t.string   "postal_code"
-    t.string   "address"
+    t.string   "street_name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "remember_me_token"
@@ -157,6 +156,7 @@ ActiveRecord::Schema.define(version: 20150322024540) do
     t.string   "activation_state"
     t.string   "activation_token"
     t.datetime "activation_token_expires_at"
+    t.integer  "street_num"
   end
 
   add_index "users", ["activation_token"], name: "index_users_on_activation_token", using: :btree
