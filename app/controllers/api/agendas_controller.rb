17809c7d98a09f9c545532e4e0235e41e1ca0d77
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-23 18:50:40 -0400

    Add Committee ID Search To API Agenda

--- app/controllers/api/agendas_controller.rb
+++ app/controllers/api/agendas_controller.rb
@@ -1,9 +1,13 @@
 class Api::AgendasController < ApiController
 	def index
-		@agendas = if @@query.empty?
+		committee_id = params[:committee_id]
+
+		@agendas = if @@query.empty? && committee_id == nil
 			paginate Agenda.all.order(change_query_order), per_page: change_per_page
-		else
+		elsif @@query.empty? == false
 			paginate Agenda.where("date = ?", Date.strptime(@@query.gsub("%",""), '%m-%d-%Y')).order(change_query_order), per_page: change_per_page
+		else
+			paginate Agenda.where("committee_id = ?", committee_id).order(change_query_order), per_page: change_per_page
 		end
 
 		render json: @agendas
