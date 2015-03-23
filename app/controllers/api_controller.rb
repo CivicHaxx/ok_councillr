ea916228163283b47f84f2baacb0438150ca84cf
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-23 09:59:35 -0400

    Add Order To API's Querying Of Database

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,11 @@
 class ApiController < ApplicationController
+	before_action :change_query_order, only: :index
+
+	@@order = :id
+
+	def change_query_order
+		unless params[:order] == nil
+			@@order = params[:order].to_sym
+		end
+	end
 end
