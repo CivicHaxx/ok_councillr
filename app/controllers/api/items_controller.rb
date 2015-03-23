f7f5d046eb94461e85796c0ce369d5975673edb5
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-23 19:10:11 -0400

    Add Item Type ID Search To API Items

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,10 +1,14 @@
 class Api::ItemsController < ApiController
   def index
-  	@items = if @@query.empty?
+    item_type_id = params[:item_type_id]
+
+  	@items = if @@query.empty? && item_type_id == nil
       Item.all.order(change_query_order)
-  	else
+    elsif @@query.empty? == false
 			Item.where("lower(title) LIKE ? OR 
         lower(origin_type) = ?", @@query, @@query.gsub("%", "")).order(change_query_order)
+    else
+      Item.where("item_type_id = ?", item_type_id).order(change_query_order)
 		end
 
 		paginate json: @items, per_page: change_per_page
