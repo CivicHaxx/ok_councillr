7b6c7839543168bdf8c1c0c53dcca036c1374a5d
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-24 07:46:24 -0400

    Change API Item Query To Use Multiple Criteria

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,15 +1,10 @@
 class Api::ItemsController < ApiController
   def index
     item_type_id = params[:item_type_id]
+    @items = Item.all.order(change_query_order)
 
-  	@items = if @@query.empty? && item_type_id == nil
-      Item.all.order(change_query_order)
-    elsif @@query.empty? == false
-			Item.where("lower(title) LIKE ? OR 
-        lower(origin_type) = ?", @@query, @@query.gsub("%", "")).order(change_query_order)
-    else
-      Item.where("item_type_id = ?", item_type_id).order(change_query_order)
-		end
+    @items = @items.where("item_type_id = ?", item_type_id).order(change_query_order) if item_type_id.present?
+    @items = Item.where("lower(title) LIKE ? OR lower(origin_type) = ?", @@query, @@query.gsub("%", "")).order(change_query_order) unless @@query.empty?
 
 		paginate json: @items, per_page: change_per_page
   end
