493106f088c78b95c45516f46050eb85c078e463
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-24 08:04:17 -0400

    Update Item API Query Use Variable Instead Model

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -3,8 +3,8 @@ class Api::ItemsController < ApiController
     item_type_id = params[:item_type_id]
     @items = Item.all
 
+    @items = @items.where("lower(title) LIKE ? OR lower(origin_type) = ?", @@query, @@query.gsub("%", "")) unless @@query.empty?
     @items = @items.where("item_type_id = ?", item_type_id) if item_type_id.present?
-    @items = Item.where("lower(title) LIKE ? OR lower(origin_type) = ?", @@query, @@query.gsub("%", "")) unless @@query.empty?
 
 		paginate json: @items.order(change_query_order), per_page: change_per_page
   end
