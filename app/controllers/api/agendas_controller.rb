de3db526bd491d4106f0e63e21c56aa4d5442200
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-24 08:17:12 -0400

    Change API Agendas Query To Use Multiple Criteria

--- app/controllers/api/agendas_controller.rb
+++ app/controllers/api/agendas_controller.rb
@@ -1,14 +1,10 @@
 class Api::AgendasController < ApiController
 	def index
 		committee_id = params[:committee_id]
+		@agendas = Agenda.all
 
-		@agendas = if @@query.empty? && committee_id == nil
-			Agenda.all
-		elsif @@query.empty? == false
-			Agenda.where("date = ?", Date.strptime(@@query.gsub("%",""), '%m-%d-%Y'))
-		else
-			Agenda.where("committee_id = ?", committee_id)
-		end
+		@agendas = Agenda.where("date = ?", Date.strptime(@@query.gsub("%",""), '%m-%d-%Y')) unless @@query.empty?
+		@agendas = Agenda.where("committee_id = ?", committee_id) if committee_id.present?
 
 		paginate json: @agendas.order(change_query_order), per_page: change_per_page
 	end
