9a0f33757b930bbf0ddf78ef33aee1b1b2850fff
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-24 11:30:07 -0400

    Change API Agenda To Use @agendas For Searching
    Change the API agenda querying to use @agendas for searching in order to
    match the reset of the API querying that are using multiple criteria.

--- app/controllers/api/agendas_controller.rb
+++ app/controllers/api/agendas_controller.rb
@@ -3,8 +3,8 @@ class Api::AgendasController < ApiController
 		committee_id = params[:committee_id]
 		@agendas = Agenda.all
 
-		@agendas = Agenda.where("date = ?", Date.strptime(@@query.gsub("%",""), '%m-%d-%Y')) unless @@query.empty?
-		@agendas = Agenda.where("committee_id = ?", committee_id) if committee_id.present?
+		@agendas = @agendas.where("date = ?", Date.strptime(@@query.gsub("%",""), '%m-%d-%Y')) unless @@query.empty?
+		@agendas = @agendas.where("committee_id = ?", committee_id) if committee_id.present?
 
 		paginate json: @agendas.order(change_query_order), per_page: change_per_page
 	end
