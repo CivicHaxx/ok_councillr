6c86a2c97c6b5e7975d0bcd3354b84c247dd4d67
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-24 11:32:40 -0400

    Change API Councillors Query To Multiple Criteria

--- app/controllers/api/councillors_controller.rb
+++ app/controllers/api/councillors_controller.rb
@@ -1,13 +1,13 @@
 class Api::CouncillorsController < ApiController
   def index
-  	@councillors = if @@query.empty?
-  		Councillor.all
-  	else
-			Councillor.where("lower(first_name) LIKE ? OR 
-        lower(last_name) LIKE ? OR 
-        lower(email) LIKE ?", @@query, @@query, @@query)
-		end
+    ward_id = params[:ward_id]
+    @councillors = Councillor.all
 
+    @councillors = @councillors.where("lower(first_name) LIKE ? OR 
+        lower(last_name) LIKE ? OR 
+        lower(email) LIKE ?", @@query, @@query, @@query) unless @@query.empty?
+    @councillors = @councillors.where("ward_id = ?", ward_id) if ward_id.present?
+		
   	paginate json: @councillors.order(change_query_order), per_page: change_per_page
   end
 
