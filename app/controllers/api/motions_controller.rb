e79c6fd4ed0979e5a8d9c89c45bd42c5fcd3f7b8
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-24 08:05:01 -0400

    Change API Motions Query To Use Multiple Criteria

--- app/controllers/api/motions_controller.rb
+++ app/controllers/api/motions_controller.rb
@@ -1,10 +1,14 @@
 class Api::MotionsController < ApiController
   def index
-  	@motions = if @@query.empty?
-  		Motion.all
-  	else
-      Motion.where("lower(amendment_text) LIKE ?", @@query)
-		end
+    councillor_id = params[:councillor_id]
+    item_id = params[:item_id]
+    motion_type_id = params[:motion_type_id]
+  	@motions = Motion.all 
+
+    @motions.where("lower(amendment_text) LIKE ?", @@query) unless @@query.empty?
+    @motions = @motions.where("councillor_id = ?", councillor_id) if councillor_id.present?
+    @motions = @motions.where("item_id = ?", item_id) if item_id.present?
+    @motions = @motions.where("motion_type_id = ?", motion_type_id) if motion_type_id.present?
 
   	paginate json: @motions.order(change_query_order), per_page: change_per_page
   end
