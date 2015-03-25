bbe32eba0f018e8ceec3a27eb3ad1f8079be7175
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-25 19:43:00 -0400

    Add Search By Controller Name In API Controller

--- app/controllers/api/councillors_controller.rb
+++ app/controllers/api/councillors_controller.rb
@@ -1,13 +1,20 @@
 class Api::CouncillorsController < ApiController
   def index
     ward_id = params[:ward_id]
+    councillor_name = params[:councillor_name]
     @councillors = Councillor.all
 
     @councillors = @councillors.where("lower(first_name) LIKE ? OR 
         lower(last_name) LIKE ? OR 
         lower(email) LIKE ?", @@query, @@query, @@query) unless @@query.empty?
     @councillors = @councillors.where("ward_id = ?", ward_id) if ward_id.present?
-		
+    
+    if councillor_name.present?
+      councillor_name = councillor_name.downcase
+
+		  @councillors = @councillors.where("lower(first_name) = ? OR lower(last_name) = ? OR lower(first_name) || ' ' || lower(last_name) = ?", councillor_name, councillor_name, councillor_name)
+    end
+
   	paginate json: @councillors.order(change_query_order), per_page: change_per_page
   end
 
