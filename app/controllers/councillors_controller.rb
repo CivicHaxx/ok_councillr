ca2817a8dbb388da45898151fdf50743ea74d174
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-26 15:51:06 -0400

    Reactor All Calculate Percent Into One Method
    Reactor all of the calculate percent so that they are all in one method
    because they all have similar code.

--- app/controllers/councillors_controller.rb
+++ app/controllers/councillors_controller.rb
@@ -7,22 +7,15 @@ class CouncillorsController < ApplicationController
 		@councillor          = Councillor.find params[:id]
 		@votes               = @councillor.councillor_votes.includes(:motion).page params[:page]
 		@rvr_votes           = @councillor.raw_vote_records. page params[:page]
-		@total_number_motion = Motion.count
-		@absences            = calculated_absence_percent(@councillor)
-		@yes_votes           = calculated_yes_percent(@councillor)
-		@no_votes            = calculated_no_percent(@councillor)
+		@absences            = calculated_percent_for("Absent", @councillor)
+		@yes_votes           = calculated_percent_for("Yes", @councillor)
+		@no_votes            = calculated_percent_for("No", @councillor)
 	end
 
 	private
-	def calculated_absence_percent(councillor)
-		(councillor.raw_vote_records.where(vote: "Absent").count.to_f / @total_number_motion) * 100
-	end
+	def calculated_percent_for(vote, councillor)
+		total_number_motion = Motion.count
 
-	def calculated_yes_percent(councillor)
-		(councillor.raw_vote_records.where(vote: "Yes").count.to_f / @total_number_motion) * 100
-	end
-	
-	def calculated_no_percent(councillor)
-		(councillor.raw_vote_records.where(vote: "No").count.to_f / @total_number_motion) * 100
+		(councillor.raw_vote_records.where(vote: vote).count.to_f / total_number_motion) * 100
 	end
 end
