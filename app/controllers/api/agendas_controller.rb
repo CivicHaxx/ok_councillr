ba1a776de443623ea26d92cae240ca0e174d33b4
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-26 12:10:29 -0400

    Add Search Date Range From API To API Agenda
    Add in the Search Date Range method from API Controlloer to API Agenda
    in order for Agenda's date to be search in a date range.

--- app/controllers/api/agendas_controller.rb
+++ app/controllers/api/agendas_controller.rb
@@ -3,7 +3,8 @@ class Api::AgendasController < ApiController
 		committee_id = params[:committee_id]
 		@agendas = Agenda.all
 
-		@agendas = @agendas.where("date = ?", Date.strptime(@@query.gsub("%",""), '%m-%d-%Y')) unless @@query.empty?
+		@agendas = search_by_date_range(@@query.gsub("%",""), @agendas, "date") unless @@query.empty?
+
 		@agendas = @agendas.where("committee_id = ?", committee_id) if committee_id.present?
 
 		paginate json: @agendas.order(change_query_order), per_page: change_per_page
