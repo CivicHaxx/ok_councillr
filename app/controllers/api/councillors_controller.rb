46617893ee3fb3916402edb9710e299bc299390c
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-26 11:51:44 -0400

    Add search_by_date_range() To API Controller
    Add search_by_date_range() to the API Controller because both the
    Councillor model which will use it to search throw start date in office
    and Agenda's date in search when agendas are in that range.
    
    Also added in the search date by range from API Controller into API
    Councillor

--- app/controllers/api/councillors_controller.rb
+++ app/controllers/api/councillors_controller.rb
@@ -1,6 +1,5 @@
 class Api::CouncillorsController < ApiController
   def index
-    date = params[:date]
     ward_id = params[:ward_id]
     councillor_name = params[:councillor_name]
     @councillors = Councillor.all
@@ -9,16 +8,7 @@ class Api::CouncillorsController < ApiController
         lower(last_name) LIKE ? OR 
         lower(email) LIKE ?", @@query, @@query, @@query) unless @@query.empty?
     @councillors = @councillors.where("ward_id = ?", ward_id) if ward_id.present?
-
-    if date.present?
-      dates = date.split(",")
-
-      @councillors = if dates.length > 1
-        @councillors.where("start_date_in_office >= ? AND start_date_in_office < ?", Date.strptime(dates[0], '%m-%d-%Y'), Date.strptime(dates[1], '%m-%d-%Y'))
-      else
-        @councillors.where("start_date_in_office = ?", Date.strptime(dates[0], '%m-%d-%Y'))
-      end
-    end
+    @councillors = search_by_date_range(params[:date], @councillors, "start_date_in_office")
 
     if councillor_name.present?
       councillor_name = councillor_name.downcase
--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -26,4 +26,21 @@ class ApiController < ApplicationController
 			orders
 		end
 	end
+
+	def search_by_date_range(date_from_url, model_search_throw, date_field_name)
+		return model_search_throw unless date_from_url.present?
+
+		dates = set_date_format(date_from_url.split(","))
+		
+		if dates.length > 1
+      model_search_throw.where("#{date_field_name} >= ? AND #{date_field_name} < ?", dates[0], dates[1])
+    else
+      model_search_throw.where("#{date_field_name} = ?", dates[0])
+    end
+	end
+
+	private
+	def set_date_format(dates)
+		dates.map { |date| Date.strptime(date, '%m-%d-%Y') }
+	end
 end
