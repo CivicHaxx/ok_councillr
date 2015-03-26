1b91a833c32fb38a90e6725248bcf3c3addf97f6
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-25 20:25:30 -0400

    Add Councillor ID Search for API Item

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,10 +1,13 @@
 class Api::ItemsController < ApiController
   def index
     item_type_id = params[:item_type_id]
+    councillor_id = params[:councillor_id]
+    
     @items = Item.all
 
     @items = @items.where("lower(title) LIKE ? OR lower(origin_type) = ?", @@query, @@query.gsub("%", "")) unless @@query.empty?
     @items = @items.where("item_type_id = ?", item_type_id) if item_type_id.present?
+    @items = @items.where("origin_id = ? AND origin_type = 'Councillor'", councillor_id) if councillor_id.present?
 
 		paginate json: @items.order(change_query_order), per_page: change_per_page
   end
