137c958ed0774384666ed9d9837736e5a4674428
CivicImages <kobewka@gmail.com>
2015-03-26 13:57:46 -0400

    shows vote summary

--- app/controllers/councillors_controller.rb
+++ app/controllers/councillors_controller.rb
@@ -4,16 +4,25 @@ class CouncillorsController < ApplicationController
 	end
 
 	def show	
-		@councillor = Councillor.find params[:id]
-		@votes = @councillor.councillor_votes.includes(:motion).page params[:page]
-		@rvr_votes = @councillor.raw_vote_records. page params[:page]
-		@absences = calculated_absence_percent(@councillor)
+		@councillor          = Councillor.find params[:id]
+		@votes               = @councillor.councillor_votes.includes(:motion).page params[:page]
+		@rvr_votes           = @councillor.raw_vote_records. page params[:page]
+		@total_number_motion = Motion.count
+		@absences            = calculated_absence_percent(@councillor)
+		@yes_votes           = calculated_yes_percent(@councillor)
+		@no_votes            = calculated_no_percent(@councillor)
 	end
 
 	private
 	def calculated_absence_percent(councillor)
-		total_number_motion = Motion.count
+		(councillor.raw_vote_records.where(vote: "Absent").count.to_f / @total_number_motion) * 100
+	end
 
-		(councillor.raw_vote_records.where(vote: "Absent").count.to_f / total_number_motion) * 100
+	def calculated_yes_percent(councillor)
+		(councillor.raw_vote_records.where(vote: "Yes").count.to_f / @total_number_motion) * 100
+	end
+	
+	def calculated_no_percent(councillor)
+		(councillor.raw_vote_records.where(vote: "No").count.to_f / @total_number_motion) * 100
 	end
 end
