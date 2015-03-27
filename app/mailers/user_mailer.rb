3b5c2491fbaf566c84d48ae74cb9af31057a4c53
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-27 12:34:35 -0400

    Remove User Activation Mailer Method, UserMailler

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,14 +3,7 @@ class UserMailer < ApplicationMailer
   # Subject can be set in your I18n file at config/locales/en.yml
   # with the following lookup:
   #
-  #   en.user_mailer.activation_needed_email.subject
-  #   en.user_mailer.reset_password_email.subject
-  def activation_needed_email(user)
-    @user = user
-    @url = activate_user_url(user.activation_token)
-    mail to: user.email, subject: "Welocme to OK Councillr"
-  end
-  
+  #   en.user_mailer.reset_password_email.subject  
   def reset_password_email(user)
   	@user = user
   	@url = edit_password_reset_url(user.reset_password_token)
