c1a0f8f311b95bd3fdf3d074caa0303f214e432a
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-29 16:53:17 -0400

    Add Helper Method To Display URL Query Help Text

--- app/controllers/api/docs_controller.rb
+++ app/controllers/api/docs_controller.rb
@@ -1,4 +1,6 @@
 class Api::DocsController < ApplicationController
+	helper_method :get_url_query_parameters_for
+
 	COUNCILLOR_JSON_PATH = Rails.root.join('db/data/api/councillors.json')
 	ITEM_JSON_PATH = Rails.root.join('db/data/api/item.json')
 
@@ -7,5 +9,16 @@ class Api::DocsController < ApplicationController
 		item_json        = ITEM_JSON_PATH.read
 		@councillors     = "<code lang=\"JSON\">#{councillors_json}</code>"
 		@item            = "<code lang=\"JSON\"> #{item_json}</code>"
+	end
+
+	def get_url_query_parameters_for(section)
+		case section
+			when :Agendas
+			when :Councillors
+			when :Committees
+			when :Items
+			when :Motions
+			when :Wards
+		end
 	end
 end
