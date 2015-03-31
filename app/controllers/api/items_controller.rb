e1b7c4f7c192af6dd2cb336edb41f468a5728011
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-31 14:44:11 -0400

    Add Synopsis Search For API Items

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -3,6 +3,7 @@ class Api::ItemsController < ApiController
     item_type_id = params[:item_type_id]
     number = params[:number]
     agenda_id = params[:agenda_id]
+    synopsis = params[:synopsis] 
     title = params[:title]
     
     @items = Item.all
@@ -11,6 +12,7 @@ class Api::ItemsController < ApiController
     @items = @items.where("item_type_id = ?", item_type_id) if item_type_id.present?
     @items = @items.where("lower(number) = ?", number.downcase) if number.present?
     @items = @items.where("lower(title) LIKE ?", "%#{title.downcase}%") if title.present?
+    @items = @items.where("lower(synopsis) LIKE ?", "%#{synopsis.downcase}%") if synopsis.present?
     @items = @items.where("origin_id = ? AND origin_type = 'Agenda'", agenda_id) if agenda_id.present?
 
 		paginate json: @items.order(change_query_order), per_page: change_per_page
