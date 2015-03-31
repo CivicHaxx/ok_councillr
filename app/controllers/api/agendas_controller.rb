10cdb74b234de512c04e3f458d0504dbf8faa0ad
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-31 12:40:16 -0400

    Add Date Search For API Agenda

--- app/controllers/api/agendas_controller.rb
+++ app/controllers/api/agendas_controller.rb
@@ -4,8 +4,8 @@ class Api::AgendasController < ApiController
 		@agendas = Agenda.all
 
 		@agendas = search_by_date_range(@@query.gsub("%",""), @agendas, "date") unless @@query.empty?
-
 		@agendas = @agendas.where("committee_id = ?", committee_id) if committee_id.present?
+    @agendas = search_by_date_range(params[:date], @agendas, "date")
 
 		paginate json: @agendas.order(change_query_order), per_page: change_per_page
 	end
