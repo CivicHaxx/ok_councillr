112bd95124690dc066ddfff668d0858aa37529db
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-31 13:09:28 -0400

    Add URL Query For Name In API Committee

--- app/controllers/api/committees_controller.rb
+++ app/controllers/api/committees_controller.rb
@@ -1,9 +1,11 @@
 class Api::CommitteesController < ApiController
 	def index
+		name = params[:name]
 		councillor_id = params[:councillor_id]
 		@committees = Committee.all
 
 		@committees = @committees.where("lower(name) LIKE ?", @@query) unless @@query.empty?
+		@committees = @committees.where("lower(name) = ?", name.downcase) if name.present?
 		@committees = @committees.joins(:committees_councillors).where("councillor_id = ?", councillor_id) if councillor_id.present?
 
 		paginate json: @committees.order(change_query_order), per_page: change_per_page
