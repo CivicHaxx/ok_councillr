3c99e53eacc949a758e90b355a3a15a38c0907b6
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-31 13:55:10 -0400

    Add Amendment Text Search For API Motion

--- app/controllers/api/motions_controller.rb
+++ app/controllers/api/motions_controller.rb
@@ -1,11 +1,13 @@
 class Api::MotionsController < ApiController
   def index
+    amendment_text = params[:amendment_text]
     councillor_id = params[:councillor_id]
     item_id = params[:item_id]
     motion_type_id = params[:motion_type_id]
   	@motions = Motion.all 
 
     @motions = @motions.where("lower(amendment_text) LIKE ?", @@query) unless @@query.empty?
+    @motions = @motions.where("lower(amendment_text) LIKE ?", "%#{amendment_text.downcase}%") if amendment_text.present?
     @motions = @motions.where("councillor_id = ?", councillor_id) if councillor_id.present?
     @motions = @motions.where("item_id = ?", item_id) if item_id.present?
     @motions = @motions.where("motion_type_id = ?", motion_type_id) if motion_type_id.present?
