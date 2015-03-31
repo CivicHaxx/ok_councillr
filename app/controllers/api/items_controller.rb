7f42342840594e6a4cfb7b98394076f288362d00
Jason Stathopulos <jstathopulos@gmail.com>
2015-03-31 16:54:32 -0400

    Add Search Into Sections For API Items

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -3,6 +3,7 @@ class Api::ItemsController < ApiController
     item_type_id = params[:item_type_id]
     number = params[:number]
     agenda_id = params[:agenda_id]
+    sections = params[:sections]
     synopsis = params[:synopsis] 
     title = params[:title]
     
@@ -12,6 +13,7 @@ class Api::ItemsController < ApiController
     @items = @items.where("item_type_id = ?", item_type_id) if item_type_id.present?
     @items = @items.where("lower(number) = ?", number.downcase) if number.present?
     @items = @items.where("lower(title) LIKE ?", "%#{title.downcase}%") if title.present?
+    @items = @items.where("lower(sections) LIKE ?", "%#{sections.downcase}%") if sections.present?
     @items = @items.where("lower(synopsis) LIKE ?", "%#{synopsis.downcase}%") if synopsis.present?
     @items = @items.where("origin_id = ? AND origin_type = 'Agenda'", agenda_id) if agenda_id.present?
 
