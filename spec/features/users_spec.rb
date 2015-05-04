5926316dd24ca6df556208047623f274725c3976
Jason Stathopulos <jstathopulos@gmail.com>
2015-05-04 15:17:29 -0400

    Add User Feature Testing & Sign Up Feature Test

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+require 'capybara/rspec'
+
+RSpec.feature "Users", type: :feature do
+	describe "#sign_up" do
+  	context "with all fields fill post" do
+  		it "responds with 200" do
+  			visit(root_url)
+	      click_link("Sign Up")
+
+	  		within "#new_user" do
+	  			user_new_password = "Pass3word:"
+					
+					fill_in "Email", with: Faker::Internet.free_email
+					fill_in "First Name", with: Faker::Name.first_name
+	  			fill_in "Last Name", with: Faker::Name.last_name
+					fill_in "Street Name", with: "King Street W."
+					fill_in "Street Number", with: 220
+					fill_in "Password", with: user_new_password
+	  			fill_in "Password Confirmation", with: user_new_password
+	  		end
+
+	  		click_on "Sign Up"
+
+	  		expect(page.status_code).to be(200)
+  		end
+  	end
+
+  	
+  end
+end
