a8c61a881b7a8e897a941bfc493efa1d8fe723e9
Jason Stathopulos <jstathopulos@gmail.com>
2015-05-04 17:46:17 -0400

    Fix Wrong User Name Field, Feature Test Auto Login
    Fix the wrong use of user name it should be email instead of username
    when running feature test to auto login a user.

--- spec/support/sorcery.rb
+++ spec/support/sorcery.rb
@@ -3,7 +3,7 @@ module Sorcery
     module Rails
       module Integration
         def login_user_post(user, password)
-          page.driver.post(user_sessions_url, { username: user, password: password}) 
+          page.driver.post(user_sessions_url, { email: user, password: password}) 
         end
       end
     end
