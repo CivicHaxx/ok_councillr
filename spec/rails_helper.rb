1c1e468c7ab3d58b61955aeaa655552b4aa46c12
Jason Stathopulos <jstathopulos@gmail.com>
2015-05-04 14:03:53 -0400

    Add Factory Girl Config Into Rspec Support Files

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -25,6 +25,8 @@ require 'rspec/rails'
 ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
+  config.include FactoryGirl::Syntax::Methods
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -47,4 +49,13 @@ RSpec.configure do |config|
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
   config.infer_spec_type_from_file_location!
+  
+  config.before(:suite) do
+    begin
+      DatabaseCleaner.start
+      FactoryGirl.lint
+    ensure
+      DatabaseCleaner.clean
+    end
+  end
 end
