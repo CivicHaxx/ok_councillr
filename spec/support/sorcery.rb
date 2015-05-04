62d67ebf229708fdc69e08499ca95beb9c4085e8
Jason Stathopulos <jstathopulos@gmail.com>
2015-05-04 14:04:43 -0400

    Add Sorcery Config For Feature Testing

---
+++ spec/support/sorcery.rb
@@ -0,0 +1,11 @@
+module Sorcery
+  module TestHelpers
+    module Rails
+      module Integration
+        def login_user_post(user, password)
+          page.driver.post(user_sessions_url, { username: user, password: password}) 
+        end
+      end
+    end
+  end
+end
